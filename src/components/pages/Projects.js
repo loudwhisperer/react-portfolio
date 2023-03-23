e9c16fc37ce4cdcf7853d5403ac2458202f8e790
loudwhisperer <driftaway44@gmail.com>
2023-03-23 08:06:05 -0600

    add keys to projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -15,6 +15,7 @@ export default function Projects() {
       description:
         "Uses the Mapbox JS Framework to utilize a map which will create a line between two points and mark the exact center between the two then using the Four Square API returns mulitple locations in that center point",
       image: markTheMiddle,
+      id: 1,
     },
     {
       name: "The Table",
@@ -22,6 +23,7 @@ export default function Projects() {
       description:
         "The Table gives users a space to create, manage and give details to a board game, video game or tabletop roleplaying game meetup and has emailing built in to notify users attached to a game of thier invite status and the detsails therein. ",
       image: theTable,
+      id: 2,
     },
     {
       name: "Weather Application",
@@ -29,6 +31,7 @@ export default function Projects() {
       description:
         "Allows users to check a five dat forecast of the current weather in their location and saves prior searches so there is no need to query again",
       image: weather,
+      id: 3,
     },
     {
       name: "JATE",
@@ -36,6 +39,7 @@ export default function Projects() {
       description:
         "A simple text editor that utilises PWA to allow an indexedDB, Application Download and offline use",
       image: jate,
+      id: 4,
     },
     {
       name: "Note Keeper",
@@ -43,12 +47,15 @@ export default function Projects() {
       description:
         "Utilizes Express JS and Sequelize to create API routes that allow users to create, save and a simplfied database (JSON DB) of their previous notes",
       image: noteKeeper,
+      id: 5,
     },
     {
       name: "Work Day Scheduler",
       link: "https://loudwhisperer.github.io/supreme-couscous-scheduler-ultra/",
-      description: "Utilizes Bootstrap CSS, Dayjs, Jquery and Local Storage to give the user the ability to scedule their workday by hour and save those times to local storage by hour.",
-      image: scheduler
+      description:
+        "Utilizes Bootstrap CSS, Dayjs, Jquery and Local Storage to give the user the ability to scedule their workday by hour and save those times to local storage by hour.",
+      image: scheduler,
+      id: 6
     },
   ];
   return (
@@ -56,7 +63,7 @@ export default function Projects() {
       <section className="flex justify-center align-center flex-wrap ml-44 mr-44 mt-44 mb-32 sm:ml-0 sm:mr-0" id="projects">
         {Item.map((project) => {
           return (
-            <Link to={project.link} target="_blank">
+            <Link to={project.link} key={project.id} target="_blank">
               <div class="max-w-sm rounded overflow-hidden shadow-lg m-4  w-4/5">
                 <img class="w-full" src={project.image} alt="..." />
                 <div class="px-6 py-4">
